1ab18c7a6bd7c580d756d7a3a73a763f5e0c8647
Abdulaziz20040 <abduazizabdushukurov2004@gmail.com>
2025-01-25 10:43:39 +0500

    add text-respos

--- src/pages/indesx.tsx
+++ src/pages/indesx.tsx
@@ -3,8 +3,8 @@ import React from "react";
 function Index() {
   return (
     <div className="flex items-center justify-center min-h-screen p-4">
-      <div className="p-4  rounded-lg shadow-inner text-center max-w-sm w-full">
-        <p className="text-lg font-semibold text-gray-500">
+      <div className="p-4 rounded-lg shadow-inner text-center max-w-sm w-full">
+        <p className="text-base sm:text-lg md:text-xl font-semibold text-gray-500">
           Ma'lumot qo'shish uchun menulardan birini tanlang
         </p>
       </div>
