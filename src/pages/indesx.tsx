74c0dce93071c081878c33f9734d2e21ec21e75d
Abdulaziz20040 <abduazizabdushukurov2004@gmail.com>
2025-01-25 10:37:36 +0500

    .

--- src/pages/indesx.tsx
+++ src/pages/indesx.tsx
@@ -2,8 +2,12 @@ import React from "react";
 
 function Index() {
   return (
-    <div className=" flex items-center justify-center h-[90vh]">
-      Malumot qo'shish uchun menulardan birini tanlang
+    <div className="flex items-center justify-center min-h-screen p-4">
+      <div className="p-4 bg-white rounded-lg shadow-inner text-center max-w-sm w-full">
+        <p className="text-lg font-semibold text-gray-800">
+          Ma'lumot qo'shish uchun menulardan birini tanlang
+        </p>
+      </div>
     </div>
   );
 }
